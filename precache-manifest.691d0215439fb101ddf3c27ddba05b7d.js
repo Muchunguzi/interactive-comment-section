e987e27ce973ad4b90bc32530a153df9d28aa4f2
muchunguzi <muchunguzikennedy@gmail.com>
2026-07-30 10:12:15 +0400

    Updates

---
+++ precache-manifest.691d0215439fb101ddf3c27ddba05b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0672c29a7e7ef68dbba585c218a6128b",
+    "url": "/interactive-comment-section/index.html"
+  },
+  {
+    "revision": "4b85cc43a8cd7f61ed9c",
+    "url": "/interactive-comment-section/static/css/main.77bc96d1.chunk.css"
+  },
+  {
+    "revision": "fde19087500b97e57fb7",
+    "url": "/interactive-comment-section/static/js/2.bfe76fb7.chunk.js"
+  },
+  {
+    "revision": "c3023434f7559cca96d097bc702ab6ae",
+    "url": "/interactive-comment-section/static/js/2.bfe76fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6093aa01f96a4535184e",
+    "url": "/interactive-comment-section/static/js/3.c8be275f.chunk.js"
+  },
+  {
+    "revision": "4b85cc43a8cd7f61ed9c",
+    "url": "/interactive-comment-section/static/js/main.8aeef961.chunk.js"
+  },
+  {
+    "revision": "a7c3c460c175cded4c86",
+    "url": "/interactive-comment-section/static/js/runtime-main.7b0c143e.js"
+  }
+]);
